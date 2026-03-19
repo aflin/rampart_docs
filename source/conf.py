9524ddd7ee92c3c3bb75295edc6abb1792f7e105
aflin <aaron@flin.org>
2026-03-19 16:52:28 -0700

    version; render

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2025, Moat Crossing Systems'
 author = 'Moat Crossing Systems'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.6.2'
 
 
 # -- General configuration ---------------------------------------------------
